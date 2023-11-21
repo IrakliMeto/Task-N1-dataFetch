d6f175c40c225bb70fbcf84fdb4586c83f9e9243
Irakli Metonidze <64604546+IrakliMeto@users.noreply.github.com>
2023-11-21 12:34:49 +0400

    ...
    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+
+interface IPost {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
 function App() {
+  const [data, setData] = useState<IPost[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const savedData = localStorage.getItem('savedData');
+    if (savedData) {
+      setData(JSON.parse(savedData));
+    } else {
+      fetchData();
+    }
+  }, []);
+
+  const fetchData = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts?size=50')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Error');
+        }
+        return response.json();
+      })
+      .then((json: IPost[]) => {
+        const slicedData = json.slice(0, 30);
+        setData(slicedData);
+        localStorage.setItem('savedData', JSON.stringify(slicedData));
+      })
+      .catch((error) => {
+        setError('Error fetching data');
+        console.error('Error fetching data:', error);
+      });
+  };
+
+  const removePost = (postId: number) => {
+    if (!data) return;
+
+    const updatedData = data.filter((item) => item.id !== postId);
+    setData(updatedData);
+    localStorage.setItem('savedData', JSON.stringify(updatedData));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={fetchData}>Fetch Data</button>
+      {data ? <h3>posts quantity: {data?.length}</h3> : null}
+
+      {error ? (
+        <p>{error}</p>
+      ) : (
+        <div>
+          {data ? (
+            <div>
+              {data.map((item) => (
+                <div key={item.id}>
+                  <h2>{item.title}</h2>
+                  <p>{item.body}</p>
+
+                  <button onClick={() => removePost(item.id)}>Remove Post</button>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+      )}
     </div>
   );
 }
